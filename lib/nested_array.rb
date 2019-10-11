d254460703abc2068362663dce31ac3ce6e0f082
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-11 18:30:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,6 +42,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  sorted_array[1][1] = "Hal Incandenza"
 end
 
 def matrix_update(matrix, row, column, new_value)
